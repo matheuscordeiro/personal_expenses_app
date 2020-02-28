6474dccee5399558738a5d48309effc586f9a4f9
Matheus Cordeiro <matheus.cordeiro@MacBook-Pro-de-Matheus.local>
2020-02-27 21:43:48 -0300

    Flutter get upgrade command executed

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/matheus.cordeiro/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/matheus.cordeiro/workspace-flutter/personal_expenses_app"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/matheus.cordeiro/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
